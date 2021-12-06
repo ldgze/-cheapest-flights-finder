ed8adbb3296afc6ec11157f2f5d7ee6ccb6d911b
lidgz <li.dongz@northeastern.edu>
2021-12-06 10:32:26 -0800

    update files

--- src/cheapestFlightsWithinKStops.java
+++ src/cheapestFlightsWithinKStops.java
@@ -1,35 +1,6 @@
 import java.util.*;
 
 public class cheapestFlightsWithinKStops {
-    // BellmanFord
-    public static int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
-        if (n == 0 || flights == null || flights.length == 0)
-            return -1;
-
-        int[] cost = new int[n];
-        Arrays.fill(cost, Integer.MAX_VALUE);
-        cost[src] = 0;
-
-        for (int i = 0; i <= K; i++) {
-            int[] currCost = cost.clone();
-
-            for (int[] flight : flights) {
-                int s = flight[0];
-                int d = flight[1];
-
-                if (cost[s] == Integer.MAX_VALUE)
-                    continue;
-
-                if (currCost[d] > cost[s] + flight[2]) {
-                    currCost[d] = cost[s] + flight[2];
-                }
-            }
-
-            cost = currCost;
-        }
-        return cost[dst] == Integer.MAX_VALUE ? -1 : cost[dst];
-    }
-
     public static void main(String[] args) {
         int[][] edges = {{0, 1, 100}, {1, 2, 100}, {0, 2, 500}};
 
@@ -37,7 +8,7 @@ public class cheapestFlightsWithinKStops {
                 {3, 6, 3}, {4, 0, 10}, {4, 6, 8}, {5, 2, 6}, {1, 4, 3}, {4, 1, 6}, {0, 5, 10}, {3, 1, 5},
                 {4, 3, 1}, {5, 4, 10}, {0, 1, 6}};
         long start = System.nanoTime();
-        System.out.println(cheapestFlightsWithinKStops.findCheapestPrice(7, e2, 2, 4, 2));
+        System.out.println(BellmanFord.findCheapestPrice(7, e2, 2, 4, 2));
         long finish = System.nanoTime();
         long timeElapsed = finish - start;
         System.out.println("Time elapsed in nanoseconds is: " + timeElapsed);
