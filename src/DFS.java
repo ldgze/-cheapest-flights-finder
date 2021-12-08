2f3a4e4442aae7beb84621d975013a6a069d2af8
lidgz <li.dongz@northeastern.edu>
2021-12-08 10:39:15 -0800

    update files

--- src/DFS.java
+++ src/DFS.java
@@ -1,2 +1,30 @@
+
 public class DFS {
+    static int INF = 1000001;
+    public static int findCheapestPrice(int n, int[][] flights, int src, int dst, int K) {
+        int cost = dfs(flights, src, dst, K + 1);
+        return cost >= INF? -1 : cost;
+    }
+
+    private static int dfs(int[][] flights, int i, int dst, int k) {
+        if (k < 0) {
+            return INF;
+        }
+
+        if (i == dst) {
+            return 0;
+        }
+
+        int min = INF;
+        for (int[] flight : flights) {
+            if (flight[0] == i) {
+                min = Math.min(min, dfs(flights, flight[1], dst, k - 1) + flight[2]);
+            }
+        }
+
+
+        return min;
+    }
+
+
 }
